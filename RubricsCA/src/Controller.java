c9f12f982ddb419924d68831b70e1ed8d0fd60e7
Mark Grogan <mark.grogan123@gmail.com>
2021-05-07 09:59:41 +0100

    get list of all rubrics + test

--- RubricsCA/src/Controller.java
+++ RubricsCA/src/Controller.java
@@ -1,23 +1,49 @@
-
 import java.util.ArrayList;
 
 public class Controller {
 	ArrayList<String> rubric =new ArrayList<String>();
-	
+	ArrayList<String> rubric1 =new ArrayList<String>();
+	ArrayList<ArrayList> rubrics = new ArrayList<>(); 
+
 	public ArrayList createRubric() {
+
+		rubric.add("Code");
+		rubric.add("Plan");
+		rubric.add("Documentation");
+		
+		rubrics.add(rubric);
+		
+		rubric1.add("Plan");
+		rubric1.add("Design");
+		rubric1.add("Testing");
+		
+		rubrics.add(rubric);
+		rubrics.add(rubric1);
+		
+		return rubric;
+	}
+
+	public ArrayList addCriterion() {
+
+		rubric.add("Design");
+
+		return rubric;
+
+	}
+	
+	public ArrayList allRubrics() {
 		
 		rubric.add("Code");
 		rubric.add("Plan");
 		rubric.add("Documentation");
-	
-		return rubric;
-	}
-	
-	public ArrayList addCriterion() {
 		
-		rubric.add("Design");
+		rubric1.add("Plan");
+		rubric1.add("Design");
+		rubric1.add("Testing");
 		
-		return rubric;
+		rubrics.add(rubric);
+		rubrics.add(rubric1);
+		return rubrics;
 		
 	}
 
--- RubricsCA/src/TestClass.java
+++ RubricsCA/src/TestClass.java
@@ -5,21 +5,54 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class TestClass {
+	ArrayList<String> rubric =new ArrayList<String>();
+	ArrayList<String> rubric1 =new ArrayList<String>();
+	ArrayList<ArrayList> rubrics = new ArrayList<>(); 
 	
+
+	Controller controller = new Controller();
 	
+	//test for creating a Rubric
 	 @Test
 	 public void testCreateRubric() {
-			ArrayList<String> rubric =new ArrayList<String>();
-			rubric.add("code");
-			rubric.add("plan");
-			rubric.add("documentation");
+
+	    rubric.add("Code");
+		rubric.add("Plan");
+		rubric.add("Documentation");
 		
-			Controller controller = new Controller();
-			
-			assertEquals(rubric,controller.createRubric());
-			
-		
+
+	
+		assertEquals(rubric,controller.createRubric());
 	
 	 }
+	
+	 //test for adding criteria
+	 @Test
+	 public void testAddriteria() {
+			
+		rubric.add("Design");
+		
+		assertEquals(rubric,controller.addCriterion());
+	
+	 }
+	 
+	//test for getting all Rubrics  
+		 @Test
+		 public void testAllRubrics() {
+		
+	    	rubric.add("Code");
+			rubric.add("Plan");
+			rubric.add("Documentation");
+				
+			rubric1.add("Plan");
+			rubric1.add("Design");
+			rubric1.add("Testing");
+			
+			rubrics.add(rubric);
+			rubrics.add(rubric1);
+				
+			assertEquals(rubrics,controller.allRubrics());
+		
+		 }
 	 
 }
